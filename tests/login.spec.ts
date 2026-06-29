a88764d0b10cf32cc92bbda60e19129b5df68df9
HussainAhmedShaik <hussainahmedshaik1996@gmail.com>
2026-06-29 12:12:27 +0530

    final

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,6 @@ test.describe('Login', () => {
   test('shows an error for invalid credentials', async ({ loginPage }) => {
     await loginPage.goto();
     await loginPage.login('standard_user', 'wrong-password');
-
     await loginPage.assertVisible(loginPage.errorMessage);
     await loginPage.assertContainsText(loginPage.errorMessage, 'Username and password do not match');
   });
